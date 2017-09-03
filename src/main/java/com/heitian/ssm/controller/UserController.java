eda0a074a1971947017c129ef0955b5ef80227ec
huangjinchao <huangjinchao@xiaomi.com>
2017-09-03 15:34:41 +0800

    just test intellij idea

--- src/main/java/com/heitian/ssm/controller/UserController.java
+++ src/main/java/com/heitian/ssm/controller/UserController.java
@@ -12,19 +12,12 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
- * Created by Zhangxq on 2016/7/15.
+ * hello this just test github of intellij idea
  */
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-
-    private Logger log = Logger.getLogger(UserController.class);
-
-    @Resource
-    private UserService userService;
-
-    @RequestMapping("/showUser")
     public String showUser(HttpServletRequest request, Model model){
         log.info("查询所有用户信息");
         List<User> userList = userService.getAllUser();
